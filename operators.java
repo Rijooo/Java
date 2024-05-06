018a87940794dfcc2293ef5a3afbe6c991e0736d
Rijooo <connectrijoben@gmail.com>
2024-05-06 21:35:57 +0530

    operator

---
+++ operators.java
@@ -0,0 +1,33 @@
+class sample 
+{
+      sample()
+      {
+        int x=10;
+      
+      }
+      public void relational(int x,int y)
+      {
+       
+        System.out.println(x<y);
+        
+
+      }
+
+      public void logical()
+      {
+        int x=20,y=20;
+        System.out.println((x>100)||(x<30));
+      }
+}
+
+
+public class operators
+ {
+    public static void  main(String das[])
+    {
+     sample obj=new sample();
+     obj.relational(100,100); 
+     obj.logical();
+     
+    }    
+}
