acf6feca78e04695c4629120017ae7ba92e6ee6a
Rijooo <connectrijoben@gmail.com>
2024-05-06 21:37:55 +0530

    if

---
+++ iff.java
@@ -0,0 +1,25 @@
+class sample 
+ {
+     public void checkmyvoting(int age)
+     {
+       
+        if(age>=18)
+        {
+            System.out.println("He/She eligible to vote");
+        }
+        else
+        {
+            System.out.println("Not eligible to vote");
+        }
+
+     }
+     
+ }
+
+public class iff{
+public static void main(String asd[])
+{
+    sample obj=new sample();
+    obj.checkmyvoting(23);
+}    
+}
