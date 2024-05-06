a7e3e097c7b48778c6fdda3775b073e4903c8931
Rijooo <connectrijoben@gmail.com>
2024-05-06 20:23:46 +0530

    datatype

---
+++ datatype.java
@@ -0,0 +1,13 @@
+class sample{
+    public void getdata(){
+        int a = 5;
+        System.out.println(a);
+    }
+}
+
+
+class datatype{
+    public static void main(String args[]){
+        new sample().getdata();
+    }
+}
