b2eb6cb024287899b3d4a043987f25a57b9d9e09
Rijooo <connectrijoben@gmail.com>
2024-05-07 20:47:24 +0530

    this

---
+++ DAY 2/thiss.java
@@ -0,0 +1,24 @@
+class football
+{
+    String name;
+    String country;
+    int goat;
+    football(String name,String country,int goat)
+    {
+        this.name=name;
+        this.country=country;
+        this.goat=goat;
+    }
+    public void football1()
+    {
+        System.out.println(name + "is" + country + "and" + goat);
+    }
+}
+
+public class thiss 
+{
+    public static void main(String args[])
+    {
+        football s1 = new football("cristiano", "portugal", 1);
+    }
+}
