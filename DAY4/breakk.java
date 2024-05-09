acd816a030d33c0b79fcf51a39efbf28767a1723
Rijooo <connectrijoben@gmail.com>
2024-05-09 21:59:57 +0530

    d

---
+++ DAY4/breakk.java
@@ -0,0 +1,8 @@
+package DAY4;
+
+public class breakk {
+    public static void main(String[] args)
+    {
+       sam
+    }
+}
---
+++ DAY4/odarray.java
@@ -0,0 +1,21 @@
+package DAY4;
+
+class sample{
+
+    int name[]= new int[5];
+    int[] name1= new int[5];
+    int []name2 = new int[6];    // can declare array like all 3 methods
+    sample(){
+
+    }
+}
+
+
+
+
+public class odarray {
+    public static void main(String[] args){
+
+        sample s1 = new sample();
+    }
+}
