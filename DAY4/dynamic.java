b3e311bf51523214c2e407e2fb81bfd07000c9d1
Rijooo <connectrijoben@gmail.com>
2024-05-09 22:31:40 +0530

    m

---
+++ DAY4/dynamic.java
@@ -0,0 +1,14 @@
+package DAY4;
+
+public class dynamic {
+    public static void main(String[] args)
+    {
+        int x[]= new int[]{1,2,3,4};
+        String y[] = new String[]{"rijo","asmi","enzo","gwen"};
+
+        for(int i=0;i<=y.length;i++)
+        {
+            System.out.println(y[i]);
+        }
+    }
+}
