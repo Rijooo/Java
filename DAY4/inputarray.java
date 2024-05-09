aab3fe1c40653a73b653dfb94623da433d65a74f
Rijooo <connectrijoben@gmail.com>
2024-05-09 22:27:01 +0530

    m

--- DAY4/inputarray.java
+++ DAY4/inputarray.java
@@ -1,7 +1,18 @@
 package DAY4;
 
+import java.io.DataInputStream;
+
 public class inputarray {
     public static void main(String args[]){
-        
+        int x[]= new int[5];
+        DataInputStream din = new DataInputStream(System.in);
+
+        for(int i=0;i<x.length;i++)
+        {
+
+             try{
+                     x[i]=Integer.parseInt(din.readLine());
+                }catch(Exception s){}
+        }        
     }
 }
