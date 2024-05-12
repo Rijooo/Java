f719b8136085cdf831b31a82d2c6417793bad82e
Rijooo <connectrijoben@gmail.com>
2024-05-12 20:24:37 +0530

    m

---
+++ DAY 5/instancee.java
@@ -0,0 +1,30 @@
+class football{
+
+      static
+      {
+            System.out.println("ronaldo");
+      }
+      void futball()
+      {
+            System.out.println("kaka");  
+      }
+      football()
+      {
+            System.out.println("ozil");
+      }
+      {
+            System.out.println("ronnie is the goat");
+      }
+
+}
+
+
+
+public class instancee {
+    public static void main(String[] args)
+     {
+            football s1 = new football();
+            football s2 = new football();
+            s2.futball();
+     }
+}
